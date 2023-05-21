5dbadf6bba480482b95568156adff2dc77a23505
amine <aminenachit98@gmail.com>
2023-05-21 22:35:46 +0100

    5

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app_admin_panel/screens/main_screen.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'consts/theme_data.dart';
 import 'controllers/MenuController.dart' as xx;
 import 'inner_screens/add_prod.dart';
 import 'providers/dark_theme_provider.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+    apiKey: "AIzaSyDO8jRdGhCxavOVhFATzcbOjUWepRJvWMU",
+    projectId: "mediashop-f69cb",
+    messagingSenderId: "937666770377",
+    appId: "1:937666770377:web:bb9207a513c02eaaaae418",
+  ));
+
   runApp(const MyApp());
 }
 
